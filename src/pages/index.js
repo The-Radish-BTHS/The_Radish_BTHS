2b398d18743e7a99a0776ab111d7a007b2e31984
Aramie <aramie.ewen@gmail.com>
2021-04-21 00:38:42 -0400

    commit??

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Link to="/articles">articles</Link>
+    </>
+  )
 }
