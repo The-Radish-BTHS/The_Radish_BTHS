af425021980ebb51b3021b782d1b2ccb63ccb960
Aramie <aramie.ewen@gmail.com>
2021-04-21 16:01:18 -0400

    ???

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,15 +2,6 @@ import React from "react"
 import Navbar from "./Navbar/Navbar"
 import "./Layout.css"
 
-// export default const Layout({ children }) => {
-//   return (
-//     <>
-//       <Navbar />
-//       <main>{children}</main>
-//     </>
-//   )
-// }
-
 export default function Layout({ children }) {
   return (
     <>
