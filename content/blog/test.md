61aa98e0481170bfc8f3acf40e663a1b308a3730
Aramie <aramie.ewen@gmail.com>
2021-04-25 22:38:24 -0400

    Changes to make GraphQl queries work??

---
+++ content/blog/test.md
@@ -0,0 +1,6 @@
+---
+title: test
+date: 2021-04-25T03:54:09.396Z
+description: test
+---
+TEST FROM CONTENT
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,27 @@
 
 module.exports = {
   /* Your site config here */
-  // siteMetadata: {
-  //   title: `The Radish`,
-  //   description: `Brooklyn Technical's first, worst, and only satirical newspaper.`
-  // },
-  plugins: [`gatsby-plugin-netlify-cms`],
+  siteMetadata: {
+    title: `The Radish`,
+    description: `Brooklyn Technical's first, worst, and only satirical newspaper.`
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-typography`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/blog/`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-typography`,
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   },
+    // },
+  ],
 }
