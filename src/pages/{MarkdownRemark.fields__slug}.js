29e2b414824fdad73e39e5b8e48d3575e6b483a4
Aramie <aramie.ewen@gmail.com>
2021-04-30 21:55:50 -0400

    Add routing for pages

---
+++ src/pages/{MarkdownRemark.fields__slug}.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <h2>{frontmatter.date}</h2>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+      }
+    }
+  }
+`
