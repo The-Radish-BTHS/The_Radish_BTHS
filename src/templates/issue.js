adf730beabaeebd713308a617f325b1add7dcfd9
Aramie <aramie.ewen@gmail.com>
2021-05-04 18:13:12 -0400

    Comment out more stuff??

--- src/templates/issue.js
+++ src/templates/issue.js
@@ -1,29 +1,29 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
-  return (
-    <Layout>
-      <h1>{frontmatter.title}</h1>
-      <h3>{frontmatter.date}</h3>
-      <a href={frontmatter.url}>PDF</a>
-    </Layout>
-  )
-}
-
-export const pageQuery = graphql`
-  query($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        title
-        url
-      }
-    }
-  }
-`
+// import React from "react"
+// import Layout from "../components/Layout"
+// import { graphql } from "gatsby"
+//
+// export default function Template({
+//   data, // this prop will be injected by the GraphQL query below.
+// }) {
+//   const { markdownRemark } = data // data.markdownRemark holds your post data
+//   const { frontmatter, html } = markdownRemark
+//   return (
+//     <Layout>
+//       <h1>{frontmatter.title}</h1>
+//       <h3>{frontmatter.date}</h3>
+//       <a href={frontmatter.url}>PDF</a>
+//     </Layout>
+//   )
+// }
+//
+// export const pageQuery = graphql`
+//   query($id: String!) {
+//     markdownRemark(id: { eq: $id }) {
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//         title
+//         url
+//       }
+//     }
+//   }
+// `
