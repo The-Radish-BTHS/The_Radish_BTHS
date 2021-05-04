e1adcbc0718c09f86a827756ef62f268b3409afb
Aramie <aramie.ewen@gmail.com>
2021-05-04 17:29:23 -0400

    Sticky nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import "./Navbar.css"
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+      const offset = window.scrollY;
+      let navbar = document.getElementById("navbar");
+      if(offset > navbar.offsetHeight ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  })
+
+  let navbarClasses = scrolled ? 'sticky' : '';
+
   return (
-    <nav>
+    <nav id="navbar" className={ navbarClasses }>
       <Link to="/">The Radish</Link>
     </nav>
   )
