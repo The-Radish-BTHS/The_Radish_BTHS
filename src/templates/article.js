9003785902de17f5b82676c94dccb4bda5c27ed7
Aramie <aramie.ewen@gmail.com>
2021-05-04 18:07:06 -0400

    Redo pages?

---
+++ src/templates/article.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <h3>{frontmatter.date}</h3>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        authors {
+          name
+          position
+        }
+      }
+    }
+  }
+`
