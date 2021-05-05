85626fd5e4a6e1c64b3f0912c80225ab094763f0
Aramie <aramie.ewen@gmail.com>
2021-05-05 15:09:52 -0400

    Fix header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,27 +3,8 @@ import { Link } from "gatsby"
 import "./Navbar.css"
 
 export default function Navbar() {
-  const [scrolled, setScrolled] = useState(false);
-
-  const handleScroll = () => {
-      const offset = window.scrollY;
-      let navbar = document.getElementById("navbar");
-      if(offset > navbar.offsetHeight ){
-        setScrolled(true);
-      }
-      else{
-        setScrolled(false);
-      }
-    }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-  })
-
-  let navbarClasses = scrolled ? 'sticky' : '';
-
   return (
-    <nav id="navbar" className={ navbarClasses }>
+    <nav id="navbar">
       <Link to="/">The Radish</Link>
     </nav>
   )
