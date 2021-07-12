e1c64369093c9dcc994a51eec05b4d8c62c23084
Aramie <aramie.ewen@gmail.com>
2021-07-12 11:12:08 -0400

    Routing for authors

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,10 +17,11 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 // https://www.gatsbyjs.com/docs/tutorial/part-seven/
 // node.fields.label == "Article" ?  path.resolve(`./src/templates/article.js`) : path.resolve(`./src/templates/issue.js`)
 exports.createPages = async ({ graphql, actions }) => {
+  // Articles and issue routing ------------------------------------------------
   const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMarkdownRemark {
+  const articles_and_issues = await graphql(`
+    query articles_and_issues {
+      allMarkdownRemark(filter: {fields: {slug: {regex: "^/issues/|/articles/"}}}) {
         edges {
           node {
             fileAbsolutePath
@@ -36,7 +37,7 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  articles_and_issues.data.allMarkdownRemark.edges.forEach(({ node }) => {
     let mindate = node.frontmatter.date.slice(0, 7);
 
     let next_month = (Number(mindate.slice(5, 7))+1).toString();
@@ -52,6 +53,38 @@ exports.createPages = async ({ graphql, actions }) => {
         slug: node.fields.slug,
         mindate: mindate,
         maxdate: maxdate,
+      },
+    })
+  })
+
+  // Author routing ------------------------------------------------------------
+  const authors = await graphql(`
+    query authors {
+      allMarkdownRemark(filter: {fields: {slug: {regex: "^/authors/"}}}) {
+        edges {
+          node {
+            fileAbsolutePath
+            fields {
+              slug
+            }
+            frontmatter {
+              title # The name
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  authors.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/author.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+        name: node.frontmatter.title,
       },
     })
   })
--- src/templates/author.js
+++ src/templates/author.js
@@ -6,31 +6,60 @@ import { graphql } from "gatsby"
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  // const { markdownRemark } = data // data.markdownRemark holds your post data
+  // const { frontmatter, html } = markdownRemark
+  const { author, articles } = data
   return (
     <Layout>
-      <h1>{frontmatter.title}</h1>
-      <h3>{"Graduating "+frontmatter.grad}</h3>
-      {
-        // <div className="frontpage">
-        //   {
-        //      Gotta somehow return the articles written by an author too
-        //   }
-        // </div
-      }
+      <h1>{author.frontmatter.title}</h1>
+      <h3>{"Graduating "+author.frontmatter.grad}</h3>
+      <div className="frontpage">
+        { // Yes this is exactly the same as in issues
+          data.articles.edges.map(({node}) => {
+            return (
+              <Articard
+                key={node.id} // We could probably unpack more to not have to use so much dot notation but
+                slug={node.fields.slug}
+                title={node.frontmatter.title}
+                excerpt={node.excerpt}
+                authors={node.frontmatter.authors}
+              />
+            )
+          })
+        }
+      </div
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
-  query authors ($slug: String!) {
-    markdownRemark(fields: {slug: {eq: $slug}}) {
+  query author_content($slug: String!, $name: String!) {
+    author: markdownRemark(fields: {slug: {eq: $slug}}) {
       html
       frontmatter {
         date
         title
         position
+      }
+    }
+    articles: allMarkdownRemark(
+      filter: {frontmatter: {authors: {elemMatch: {name: {eq: $name}}}}}
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 200)
+          frontmatter {
+            title
+            authors {
+              name
+              position
+            }
+          }
+          fields {
+            slug
+          }
+        }
       }
     }
   }
