7ffe0831cc2020ab45a9578285aac4e80264d347
SantiagoVira <8bitharrypotter@gmail.com>
2021-07-13 19:30:50 -0400

    collapsible sidebar except V1 and also bad

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,27 @@ import "./Navbar.css"
 
 import Radamir from "./Radamir"
 
-export default function Navbar() {
+function CloseHamburger({ showSidebar, setShowSidebar }) {
+  return (
+    <div className="closeBurger">
+      <input
+        type="checkbox"
+        id="toggle"
+        checked={showSidebar}
+        onClick={() => setShowSidebar(!showSidebar)}
+      ></input>
+      <label htmlFor="toggle" />
+    </div>
+  )
+}
+
+export default function Navbar({ setShowSidebar, showSidebar }) {
   return (
     <nav id="navbar">
+      <CloseHamburger
+        setShowSidebar={setShowSidebar}
+        showSidebar={showSidebar}
+      />
       <Link to="/">
         <Radamir /> The Radish
       </Link>
