5825d1465148115aa59397cab518fdd00155843f
SantiagoVira <8bitharrypotter@gmail.com>
2021-07-13 20:27:49 -0400

    StickyState

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,8 +4,19 @@ import Sidebar from "./Sidebar/Sidebar"
 
 import "./Layout.css"
 
+function useStickyState(defaultValue, key) {
+  const [value, setValue] = React.useState(() => {
+    const stickyValue = window.localStorage.getItem(key)
+    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue
+  })
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+  return [value, setValue]
+}
+
 export default function Layout({ children }) {
-  const [showSidebar, setShowSidebar] = useState(true)
+  const [showSidebar, setShowSidebar] = useStickyState(false, showSidebar)
   return (
     <>
       <title>The Radish</title>
