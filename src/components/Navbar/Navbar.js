ecbde8d65c442e494bc3632647fd8b5f59d7931f
Aramie <aramie.ewen@gmail.com>
2021-08-06 20:15:15 -0400

    How to make your nav text back in three quick and easy steps (make navbar more responsive)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import "./Navbar.css"
 
@@ -24,38 +24,64 @@ export function Hamburger({ showSidebar, setShowSidebar }) {
   )
 }
 
-// Use Radimir instead of name on small screens
 export default function Navbar({ setShowSidebar, showSidebar, setShowModal, showModal }) {
-  // Logic for making the navbar change on scroll
-  // const handleResize = () => {
-  //     const offset = window.scrollY;
-  //     let navbar = document.getElementById("navbar");
-  //     if(offset > navbar.offsetHeight ){
-  //       setScrolled(true);
-  //     }
-  //     else{
-  //       setScrolled(false);
-  //     }
-  //   }
-  //
-  // useEffect(() => {
-  //   window.addEventListener('resize', handleResize)
-  // })
+  const [scrollPos, setScrollPos] = useState(0);
+  const [scrollingDown, setScrollingDown] = useState(false);
+
+  const [showName, setShowName] = useState(true);
+
+  const handleResize = () => {
+      const innerWidth =  window.innerWidth;
+
+      // Make name disappear if someone has a super tiny screen
+      if (innerWidth <= 300) {
+        setShowName(false)
+      }
+      else {
+        setShowName(true)
+      }
+    }
+
+    const handleScroll = () => {
+        const offset = window.pageYOffset || document.documentElement.scrollTop;
+        // If we were changing when the header detaches from the top
+        // let navbar = document.getElementById("navbar");
+        // if (offset > navbar.offsetHeight ){
+        //   setScrolled(true);
+        // }
+        // else{
+        //   setScrolled(false);
+        // }
+
+        // For some reason it only seems to work when we check for showSidebar in both statements??
+        if (offset > scrollPos && !showSidebar) {
+          setScrollingDown(true)
+        }
+        else if (offset < scrollPos || showSidebar) {
+          setScrollingDown(false)
+        }
+        setScrollPos(offset <= 0 ? 0 : offset)
+      }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    window.addEventListener('scroll', handleScroll)
+  })
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${scrollingDown ? "nobar" : ""}`}>
       <Hamburger
         setShowSidebar={setShowSidebar}
         showSidebar={showSidebar}
       />
-      <Link to="/" id="name"><Radimir />The Radish</Link>
+      <Link to="/" id="name"><Radimir />{`${showName ? "The Radish" : ""}`}</Link>
       <div
         role="button"
         tabIndex={0}
         className=""
         onClick={() => {
           setShowModal(!showModal)
-          setShowSidebar(!showSidebar)
+          setShowSidebar(false)
         }}
         onKeyDown={(ev) => ev.keyCode===13 ? setShowModal(!showModal) : ""}
       >
