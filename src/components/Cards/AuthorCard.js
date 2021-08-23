7bf4b3e0ca164b8a551a8924f76716686c3e8fa1
SantiagoVira <8bitharrypotter@gmail.com>
2021-08-23 15:48:53 -0400

    touching up a few things, adding employee stamp

--- src/components/Cards/AuthorCard.js
+++ src/components/Cards/AuthorCard.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./Cards.css"
+import EmployeeStamp from "./EmployeeStamp"
 
 export default function AuthorCard(props) {
   const today = new Date()
-  const grad =
-    today.getMonth() > 6 && today.getFullYear() >= props.date
+  const grad = today.getMonth() > 6 && today.getFullYear() >= props.date
   return (
     <div className="card">
+      {true && <EmployeeStamp />}
       <Link to={props.slug}>
         <h4>
-          <i>{((grad || props.former) ? "former " : "") + props.position}</i>
+          <i>{(grad || props.former ? "former " : "") + props.position}</i>
         </h4>
         <h2>{props.title}</h2>
         <p>{props.description}</p>
