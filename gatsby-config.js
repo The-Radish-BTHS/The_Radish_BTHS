ddf48bc580e064e47f3b1c26fb5a096443729fb3
Aramie <aramie.ewen@gmail.com>
2021-10-04 22:43:16 -0400

    Google please say hi

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-sitemap`,
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/googlec8914e3a66ad3d27.html.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function googlec8914e3a66ad3d27() {
-  return <body dangerouslySetInnerHTML={{ __html: "google-site-verification: googlec8914e3a66ad3d27.html" }} />;
-}
