277967ae2326312232e12a44f6fdeba5d72a1212
SantiagoVira <santiagovira06@gmail.com>
2022-08-22 03:33:12 -0400

    add all pages

---
+++ src/pages/issues/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "@components/layout/layout";
+import { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return <Layout pageIndex={1}></Layout>;
+};
+
+export default Index;
