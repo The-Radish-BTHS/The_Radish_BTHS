bd5e34a66d46dddce3f61aa7c681892387a70c60
SantiagoVira <santiagovira06@gmail.com>
2022-09-11 22:10:49 -0400

    Latest section on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import Layout from "@components/layout/layout";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Flex flexDirection="column">
+        <Heading fontSize="2rem">Latest & Greatest</Heading>
+      </Flex>
+    </Layout>
+  );
 };
 
 export default Home;
