63ae290ccba8df99e58aa6a45832694eab972704
SantiagoVira <santiagovira06@gmail.com>
2022-10-08 10:44:17 -0400

    delete backend

--- backend/test/authentication.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from 'assert';
-import app from '../src/app';
-
-describe('authentication', () => {
-  it('registered the authentication service', () => {
-    assert.ok(app.service('authentication'));
-  });
-  
-});
