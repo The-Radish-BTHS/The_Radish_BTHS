abc68e4a29c745cc073dfd0be165a6dc4d93ed5f
SantiagoVira <santiagovira06@gmail.com>
2022-10-08 21:32:40 -0400

    Setup article page

--- frontend/src/pages/articles/[id].tsx
+++ frontend/src/pages/articles/[id].tsx
@@ -1,7 +1,41 @@
+import { Flex, Text } from "@chakra-ui/react";
 import Layout from "@components/layout/layout";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
 
-const Id: React.FC = () => {
-  return <Layout></Layout>;
+const Article: NextPage<{ id: string }> = ({ id }) => {
+  // const router = useRouter();
+  // const { id } = router.query;
+  console.log(id);
+
+  return (
+    <Layout>
+      <Flex background="red" w="10vw" h="10vw" />
+    </Layout>
+  );
 };
 
-export default Id;
+export default Article;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id;
+  const title = "10 instances of phenomenal reporting by the survey";
+  const content = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+  const authorId = "abcd";
+
+  return {
+    props: {
+      id,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const data = await getData();
+  // const pathsWithParams = data.stars.map((star: starInterface) => ({ params: { id: "abcd"}}))
+
+  return {
+    paths: [{ params: { id: "abcd" } }],
+    fallback: true,
+  };
+};
