0ff2a79bf660a5fb236f4d55afbd0ce690affa4e
SantiagoVira <santiagovira06@gmail.com>
2022-10-08 22:26:46 -0400

    Adjust article page content

--- frontend/src/pages/articles/[id].tsx
+++ frontend/src/pages/articles/[id].tsx
@@ -43,7 +43,18 @@ const Article: NextPage<ArticleType> = ({
           <CardTag {...tag} key={i} />
         ))}
       </Flex>
-      <Text>{content}</Text>
+      <Text
+        textAlign="justify"
+        fontSize="clamp(16px,12px + .5vw,1.25rem)"
+        maxW={{ base: "95vw", md: "70vw" }}
+        mt="2rem">
+        {content.split("\n").map((text, i) => (
+          <span key={i}>
+            {text}
+            <br />
+          </span>
+        ))}
+      </Text>
     </Layout>
   );
 };
@@ -53,8 +64,15 @@ export default Article;
 export const getStaticProps: GetStaticProps = async (context) => {
   const id = context.params?.id;
   const title = "10 instances of phenomenal reporting by the survey";
-  const content =
-    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+  const content = `You wake up, you eat your bagel with toast, and you pat your ferret three times before hopping onto a crowded subway car. You cram your body into it at the last minute because you’re just that eager to learn. 
+
+    The train groans merrily along. Then, you stop. It's the third to last stop and this person with a green North Face puffer is getting on. Their coat is North face because they're basic—but green because they’re not that basic. Their bright yellow Fjallraven kanken backpack is way too brightly colored and definitely too small to fit all their necessities (notebooks, rocks, organs, etc.) The train is cramped and they have to stand. They don’t deserve a seat though. They would probably put their blinding, stupid backpack on their lap if they had a chance instead of the floor where it belongs.
+    
+    This person is here every day. It’s like they are following you. Maybe they are just also “going to school,” but almost everyone has an ulterior motive these days and people with white Converse are never to be trusted. 
+    
+    The train lurches and so does your mysterious stalker. 
+    
+    You check your watch, a knockoff fit bit because who has the money for those things, and see that you indeed have 3 minutes until school starts.`;
 
   const authors = [
     {
