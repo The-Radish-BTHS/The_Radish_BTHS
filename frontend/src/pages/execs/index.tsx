f1ab83ee172528fb5a7d924e7e03403aa73438c2
SantiagoVira <santiagovira06@gmail.com>
2022-10-22 16:36:20 -0400

    Add content to /execs

--- frontend/src/pages/execs/index.tsx
+++ frontend/src/pages/execs/index.tsx
@@ -1,26 +1,36 @@
+import { PersonCardType } from "@/types/person";
 import PersonCard from "@components/cards/person-card";
 import Layout from "@components/layout/layout";
+import MasonryLayout from "@components/shared/masonry/masonry-layout";
 import { GetStaticProps, NextPage } from "next";
 
-const Index: NextPage = () => {
+const Execs: NextPage<{ execs: PersonCardType[] }> = ({ execs }) => {
   return (
-    <Layout pageIndex={3}>
-      <PersonCard
-        name="Santiago Vira"
-        title="artist"
-        description='"The Cheese"'
-        id="abcd"
-        isExec
-        image="/images/aramie.webp"
-      />
+    <Layout pageIndex={3} alignItems="center">
+      <MasonryLayout>
+        {execs.map((exec, i) => (
+          <PersonCard {...exec} key={i} styles={{ mb: "2rem" }} />
+        ))}
+      </MasonryLayout>
     </Layout>
   );
 };
 
-export default Index;
+export default Execs;
 
 export const getStaticProps: GetStaticProps = async (context) => {
+  const sample = {
+    name: "Santiago M. Vira",
+    title: "Website Manager",
+    isExec: true,
+    description: "just a cheesy guy in a corny world",
+    image: "/images/aramie.webp",
+    id: "slay",
+  };
+
+  const execs = new Array(20).fill(sample);
+
   return {
-    props: {},
+    props: { execs },
   };
 };
