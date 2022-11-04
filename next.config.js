56b0c94f51ba4fbdac19dbbe17849be234e38d91
SantiagoVira <santiagovira06@gmail.com>
2022-11-04 10:37:26 -0400

    i tried superjson

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
+module.exports = {
+  ...nextConfig,
+};
 
-module.exports = nextConfig
+/*
+
+  experimental: {
+    swcPlugins: [["next-superjson-plugin", {}]],
+  },
+  */
