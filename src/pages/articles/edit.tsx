e800b519a35708cd23ed2ac9ab17ecd3057887f4
SantiagoVira <santiagovira06@gmail.com>
2022-11-27 20:26:08 -0500

    make them files

---
+++ src/pages/articles/edit.tsx
@@ -0,0 +1,235 @@
+import { Flex, Heading, Text, useDisclosure, useToast } from "@chakra-ui/react";
+import Link from "next/link";
+
+import { Person, UserPermission, Topic } from "@prisma/client";
+import { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+import styles from "@components/pages/submit/styles.module.css";
+
+import Layout from "@components/layout/layout";
+import Button from "@components/button";
+import { articleNameIsUnique } from "@lib/helpers.server";
+import RequiredUserWrapper from "@components/required-user-wrapper";
+import { trpc } from "@lib/trpc";
+import { useRouter } from "next/router";
+import StyledMultiselect from "@components/pages/submit/styled-multiselect";
+import NewTopicModal from "@components/pages/submit/new-topic-modal";
+import { ErrorMessage } from "@hookform/error-message";
+import SubmitModal from "@components/pages/submit/submit-modal";
+import { useIsMobile } from "@hooks/useIsMobile";
+
+export type InputData = {
+  title: string;
+  content: string;
+};
+
+const Edit: NextPage = () => {
+  // Get User Data
+  const { data: sessionData } = useSession();
+  const toast = useToast();
+  const mobile = useIsMobile();
+  const router = useRouter();
+  const newTopicDisclosure = useDisclosure();
+  const submitDisclosure = useDisclosure();
+
+  const articleQuery = trpc.submission.get.useQuery({
+    id: router.query.id?.toString() as string,
+  });
+  const articleSlugsQuery = trpc.article.getSlugs.useQuery();
+  const topicsQuery = trpc.topic.getAll.useQuery();
+  const topicSlugsQuery = trpc.topic.getSlugs.useQuery();
+  const peopleQuery = trpc.person.getAll.useQuery({ includeIsFormer: false });
+
+  const articleSlugs = articleSlugsQuery.data || [];
+  const topics = topicsQuery.data || [];
+  const topicSlugs = topicSlugsQuery.data || [];
+  const people = peopleQuery.data || [];
+  const article = articleQuery.data;
+
+  const submitArticle = trpc.article.editorSubmit.useMutation({
+    onError(err) {
+      toast({
+        title: `Article Edit Error ${err.data?.httpStatus}: ${err.message}`,
+        status: "error",
+        duration: 4000,
+        position: "bottom-right",
+        isClosable: true,
+      });
+    },
+    onSuccess() {
+      toast({
+        title: "Article Edited Successfully!",
+        status: "success",
+        duration: 4000,
+        position: "bottom-right",
+        isClosable: true,
+      });
+    },
+  });
+
+  // State
+  const [topicSelections, setTopicSelections] = useState<Topic[]>(
+    article?.topics || []
+  );
+  const [authorSelections, setAuthorSelections] = useState<Person[]>(
+    article?.authors || []
+  );
+  const [formData, setFormData] = useState<InputData>({
+    title: "",
+    content: "",
+  });
+
+  // React Hook Form
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm<InputData>({
+    criteriaMode: "all",
+    defaultValues: {
+      title: article?.title || "",
+      content: article?.link || "",
+    },
+  });
+
+  useEffect(() => {
+    if (
+      sessionData &&
+      sessionData?.user?.permission !== UserPermission.NORMIE
+    ) {
+      setValue("title", article?.title || "");
+      setValue("content", article?.link || "");
+      setTopicSelections(article?.topics || []);
+      setAuthorSelections(article?.authors || []);
+    }
+  }, [sessionData, article, setValue]);
+
+  return (
+    <Layout title="Edit an Article!" alignItems="center">
+      {sessionData &&
+      sessionData?.user?.permission !== UserPermission.NORMIE ? (
+        <RequiredUserWrapper>
+          <SubmitModal
+            disclosure={submitDisclosure}
+            data={formData}
+            onClick={async (inputData: InputData) => {
+              await submitArticle
+                .mutateAsync({
+                  title: inputData.title,
+                  content: inputData.content,
+                  authors: authorSelections,
+                  topics: topicSelections,
+                })
+                .catch(() => 0);
+            }}
+          />
+
+          <Heading textAlign="center">So you want to edit an Article?</Heading>
+          <Text textAlign="center" fontSize="1.25rem">
+            Thanks!
+          </Text>
+          <form
+            autoComplete="off"
+            onKeyDown={(e) => {
+              e.key === "Enter" && e.preventDefault();
+            }}
+            onSubmit={handleSubmit((data) => {
+              submitDisclosure.onOpen();
+              setFormData(data);
+            })}
+            className={styles["form-wrapper"]}
+            style={{ width: mobile ? "85vw" : "60vw" }}>
+            <NewTopicModal
+              disclosure={newTopicDisclosure}
+              topicSlugs={topicSlugs}
+              addTopic={(topic: Topic) =>
+                setTopicSelections((topics) => [...topics, topic])
+              }
+            />
+            <p>
+              Article title:<span style={{ color: "red" }}> *</span>
+            </p>
+            <input
+              placeholder="Title"
+              required
+              {...register("title", {
+                required: true,
+                validate: (data) =>
+                  articleNameIsUnique(data, articleSlugs ?? []),
+              })}
+            />
+            <p
+              className={`${styles["form-element-margin"]} ${styles["error-message"]}`}>
+              <ErrorMessage
+                errors={errors}
+                name="title"
+                render={({ messages }) => {
+                  console.log("messages", messages);
+                  return messages
+                    ? Object.entries(messages).map(([type, message], i) => (
+                        <span key={i}>
+                          {message}
+                          <br />
+                        </span>
+                      ))
+                    : null;
+                }}
+              />
+            </p>
+
+            <p>Topics covered:</p>
+            <StyledMultiselect
+              options={topics}
+              values={topicSelections}
+              setValues={setTopicSelections}
+              marginBottom={false}
+            />
+
+            <button
+              onClick={newTopicDisclosure.onOpen}
+              type="button"
+              className={styles.bottomMargin}>
+              + Add new topic
+            </button>
+
+            <p>Authors:</p>
+            <StyledMultiselect
+              options={people}
+              values={authorSelections}
+              setValues={setAuthorSelections}
+            />
+
+            <p>
+              Content:<span style={{ color: "red" }}> *</span>
+            </p>
+            <input
+              placeholder="Add the articul!"
+              required
+              {...register("content", {
+                required: true,
+              })}
+            />
+            <Button type="submit" mt="1rem">
+              Submit it!
+            </Button>
+          </form>
+        </RequiredUserWrapper>
+      ) : (
+        <Flex
+          h="100%"
+          justifyContent="center"
+          alignItems="center"
+          flexDir="column">
+          <Heading>You can&apos;t edit!</Heading>
+          <Link href="/about">Apply to join the editing team!</Link>
+        </Flex>
+      )}
+    </Layout>
+  );
+};
+
+export default Edit;
--- src/pages/articles/submit.tsx
+++ src/pages/articles/submit.tsx
@@ -1,9 +1,25 @@
-import { UserPermission } from "@prisma/client";
+import { UserPermission, Person, Topic } from "@prisma/client";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import SubmitEdited from "@components/pages/submit/submit-edited";
-import ArticleSubmit from "@components/pages/submit/article-submit";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+import styles from "@components/pages/submit/styles.module.css";
+
+import { Flex, Heading, Text, useDisclosure, useToast } from "@chakra-ui/react";
+import { articleNameIsUnique } from "@lib/helpers.server";
+import { trpc } from "@lib/trpc";
+import { ErrorMessage } from "@hookform/error-message";
+
+import Layout from "@components/layout/layout";
+import Button from "@components/button";
+import RequiredUserWrapper from "@components/required-user-wrapper";
+import StyledMultiselect from "@components/pages/submit/styled-multiselect";
+import NewTopicModal from "@components/pages/submit/new-topic-modal";
+import InfoTooltip from "@components/info-tooltip";
+import SubmitModal from "@components/pages/submit/submit-modal";
+import { useIsMobile } from "@hooks/useIsMobile";
 
 export type InputData = {
   title: string;
@@ -11,15 +27,221 @@ export type InputData = {
 };
 
 const Submit: NextPage = () => {
+  // Get Data
   const { data: sessionData } = useSession();
-  const router = useRouter();
+  const toast = useToast();
+  const mobile = useIsMobile();
+  const newTopicDisclosure = useDisclosure();
+  const submitDisclosure = useDisclosure();
 
-  const isEditing =
-    sessionData &&
-    sessionData?.user?.permission !== UserPermission.NORMIE &&
-    router.query.m?.toString() === "1";
+  const articleSlugsQuery = trpc.article.getSlugs.useQuery();
+  const topicsQuery = trpc.topic.getAll.useQuery();
+  const topicSlugsQuery = trpc.topic.getSlugs.useQuery();
+  const peopleQuery = trpc.person.getAll.useQuery({ includeIsFormer: false });
 
-  return isEditing ? <SubmitEdited /> : <ArticleSubmit />;
+  const articleSlugs = articleSlugsQuery.data || [];
+  const topics = topicsQuery.data || [];
+  const topicSlugs = topicSlugsQuery.data || [];
+  const people = peopleQuery.data || [];
+
+  const submitArticle = trpc.article.submit.useMutation({
+    onError(err) {
+      toast({
+        title: `Article Submit Error ${err.data?.httpStatus}: ${err.message}`,
+        status: "error",
+        duration: 4000,
+        position: "bottom-right",
+        isClosable: true,
+      });
+    },
+    onSuccess() {
+      toast({
+        title: "Article Submit Success!",
+        status: "success",
+        duration: 4000,
+        position: "bottom-right",
+        isClosable: true,
+      });
+    },
+  });
+
+  // State
+  const [topicSelections, setTopicSelections] = useState<Topic[]>([]);
+  const [authorSelections, setAuthorSelections] = useState<Person[]>(
+    sessionData?.user?.person ? [sessionData?.user?.person] : []
+  );
+  const [formData, setFormData] = useState<InputData>({
+    title: "",
+    content: "",
+  });
+
+  useEffect(() => {
+    setAuthorSelections(
+      sessionData?.user?.person ? [sessionData?.user?.person] : []
+    );
+  }, [sessionData]);
+
+  // React Hook Form
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<InputData>({
+    criteriaMode: "all",
+  });
+
+  return (
+    <RequiredUserWrapper>
+      <Layout title="Submit an Article!" alignItems="center">
+        <SubmitModal
+          disclosure={submitDisclosure}
+          data={formData}
+          onClick={async (inputData: InputData) => {
+            if (!sessionData?.user?.person.slug) return;
+            await submitArticle
+              .mutateAsync({
+                title: inputData.title,
+                link: inputData.content,
+                authors: [
+                  { slug: sessionData.user.person.slug },
+                  ...authorSelections,
+                ],
+                topics: topicSelections,
+              })
+              .catch(() => 0);
+          }}
+        />
+
+        <NewTopicModal
+          disclosure={newTopicDisclosure}
+          topicSlugs={topicSlugs ?? []}
+          addTopic={(topic: Topic) =>
+            setTopicSelections((topics) => {
+              console.log("topics", [...topics, topic]);
+              return [...topics, topic];
+            })
+          }
+        />
+
+        <Heading textAlign="center">So you want to submit an Article?</Heading>
+        <Text textAlign="center" fontSize="1.25rem">
+          Do it! Submit it! Go!
+        </Text>
+        <form
+          autoComplete="off"
+          onKeyDown={(e) => {
+            e.key === "Enter" && e.preventDefault();
+          }}
+          onSubmit={handleSubmit((data) => {
+            submitDisclosure.onOpen();
+            setFormData(data);
+          })}
+          className={styles["form-wrapper"]}
+          style={{ width: mobile ? "85vw" : "60vw" }}>
+          <Flex w="100%" justifyContent="space-between">
+            <p>
+              Google Docs link:<span style={{ color: "red" }}> *</span>
+            </p>
+
+            <InfoTooltip text="Be sure to share this document with theradishbths@gmail.com!!! We can't edit it if you don't :(" />
+          </Flex>
+
+          <input
+            required
+            placeholder="Google Docs Link"
+            {...register("content", {
+              required: true,
+              pattern: {
+                value:
+                  /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+                message: "That doesn't seem like a valid link!!!",
+              },
+            })}
+          />
+          <p
+            className={`${styles["form-element-margin"]} ${styles["error-message"]}`}>
+            <ErrorMessage
+              errors={errors}
+              name="content"
+              render={({ messages }) => {
+                console.log("messages", messages);
+                return messages
+                  ? Object.entries(messages).map(([type, message], i) => (
+                      <span key={i}>
+                        {message}
+                        <br />
+                      </span>
+                    ))
+                  : null;
+              }}
+            />
+          </p>
+
+          <p>
+            Article title:<span style={{ color: "red" }}> *</span>
+          </p>
+
+          <input
+            required
+            placeholder="Title"
+            {...register("title", {
+              required: true,
+              validate: (data) => articleNameIsUnique(data, articleSlugs ?? []),
+            })}
+          />
+
+          <p
+            className={`${styles["form-element-margin"]} ${styles["error-message"]}`}>
+            <ErrorMessage
+              errors={errors}
+              name="title"
+              render={({ messages }) => {
+                console.log("messages", messages);
+                return messages
+                  ? Object.entries(messages).map(([type, message], i) => (
+                      <span key={i}>
+                        {message}
+                        <br />
+                      </span>
+                    ))
+                  : null;
+              }}
+            />
+          </p>
+
+          <p>Topics covered:</p>
+          <StyledMultiselect
+            options={topics}
+            values={topicSelections}
+            setValues={setTopicSelections}
+            marginBottom={false}
+          />
+          <button
+            onClick={newTopicDisclosure.onOpen}
+            type="button"
+            className={styles.bottomMargin}>
+            + Add new topic
+          </button>
+
+          <Flex w="100%" justifyContent="space-between">
+            <p>Authors:</p>
+
+            <InfoTooltip text="Did you work with anyone on this article? Add them here! Don't worry, you're here by default" />
+          </Flex>
+          <StyledMultiselect
+            options={people?.filter(
+              (person) => person.slug !== sessionData?.user?.person.slug
+            )}
+            values={authorSelections}
+            setValues={setAuthorSelections}
+          />
+          <Button type="submit" mt="1rem">
+            Submit it!
+          </Button>
+        </form>
+      </Layout>
+    </RequiredUserWrapper>
+  );
 };
 
 export default Submit;
