abe64f3026cf59221eb8f029fef478b38e13af10
SantiagoVira <santiagovira06@gmail.com>
2022-12-14 00:02:05 -0500

    add a little more effort on styling also sleep

--- src/components/MdEditor.tsx
+++ src/components/MdEditor.tsx
@@ -1,11 +1,13 @@
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
+import { useIsMobile } from "@hooks/useIsMobile";
 
 const MdEditor: React.FC = () => {
+  const isMobile = useIsMobile();
   return (
     <Editor
       placeholder="Hello the Radish!"
-      previewStyle="vertical"
+      previewStyle={isMobile ? "tab" : "vertical"}
       height="600px"
       initialEditType="markdown"
       useCommandShortcut={true}
