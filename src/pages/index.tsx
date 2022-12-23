3844fd225b0f864e72c57c70ff31a6a70e0fc0cd
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-12-23 18:09:13 -0500

    ssg /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,35 @@ import LatestArticles from "@components/latest/latest-articles";
 import LatestSection from "@components/latest/latest-section";
 import TopicsSection from "@components/latest/topics-section";
 import Layout from "@components/layout/layout";
-import type { NextPage } from "next";
+import { getSsgCaller } from "@lib/ssg-helper";
+import type { GetStaticProps, NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <Layout alignItems="center" gap="2.5rem">
       <LatestSection />
       <LatestArticles />
-
       <TopicsSection />
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = await getSsgCaller();
+
+  await ssg.issue.getLast.prefetch();
+  await ssg.article.getAll.prefetch({
+    sortOrder: "desc",
+    take: 6,
+    exclude: [],
+  });
+  await ssg.topic.getAll.prefetch();
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+  };
 };
 
 export default Home;
