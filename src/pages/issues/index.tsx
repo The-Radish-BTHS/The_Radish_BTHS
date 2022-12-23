a52951525ec49c95be811fdca0eb25b7715af95d
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-12-23 17:44:07 -0500

    infinite scrolling issues

--- src/pages/issues/index.tsx
+++ src/pages/issues/index.tsx
@@ -1,26 +1,56 @@
-import { IssueCardType } from "@/types/issue";
 import { Heading, Text } from "@chakra-ui/react";
 import IssueCard from "@components/cards/issue-card";
 import Layout from "@components/layout/layout";
 import MasonryLayout from "@components/masonry/masonry-layout";
 import { GetStaticProps, NextPage } from "next";
-import { getIssues } from "@lib/getters/many-getters.server";
 import { trpc } from "@lib/trpc";
+import { OnBottom } from "@components/on-bottom";
+import { getSsgCaller } from "@lib/ssg-helper";
 
 const Issues: NextPage = () => {
-  const issueQuery = trpc.issue.getAll.useQuery({});
-  const issues = issueQuery.data ?? [];
+  const issuesQuery = trpc.issue.getInfinite.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: (current) => current.nextCursor,
+    }
+  );
+
+  const issues = issuesQuery.data?.pages.map((page) => page.issues).flat();
+
   return (
     <Layout pageIndex={1} textAlign="center" alignItems="center">
       <Heading>We&apos;ve got issues</Heading>
       <Text mb="3rem">Now you&apos;ve got &apos;em too</Text>
-      <MasonryLayout numItems={issues?.length}>
-        {issues.map((issue, i) => (
-          <IssueCard {...issue} key={i} styles={{ mb: "2rem" }} />
-        ))}
-      </MasonryLayout>
+
+      <OnBottom
+        onBottom={() => {
+          issuesQuery.fetchNextPage();
+        }}
+      >
+        <MasonryLayout numItems={issues?.length}>
+          {issues?.map((issue, i) => (
+            <IssueCard {...issue} key={i} styles={{ mb: "2rem" }} />
+          ))}
+        </MasonryLayout>
+      </OnBottom>
+
+      {issuesQuery.hasNextPage ? (
+        <Text>Loading more issues...</Text>
+      ) : (
+        <Text>You reached the end.</Text>
+      )}
     </Layout>
   );
 };
 
 export default Issues;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const ssg = await getSsgCaller();
+
+  await ssg.issue.getInfinite.prefetchInfinite({});
+
+  return {
+    props: { trpcState: ssg.dehydrate() },
+  };
+};
