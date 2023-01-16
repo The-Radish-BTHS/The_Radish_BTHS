4e25da35fc0547c1998e912b88e7d878a4dcb26f
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-01-16 10:01:44 -0500

    deleting my accounts

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,11 +1,23 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import DataInput from "@components/pages/account/data-input";
 import Layout from "@components/layout/layout";
 import ExecStamp from "@components/exec-stamp";
 import { getPeopleSlugs } from "@lib/getters/many-getters.server";
 import { customSlugify } from "@lib/helpers.server";
 import { GetServerSideProps, NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useRef, useState } from "react";
 import RequiredUserWrapper from "@components/required-user-wrapper";
 import { trpc } from "@lib/trpc";
@@ -21,6 +33,8 @@ const Account: NextPage<{ peopleSlugs: string[] }> = ({ peopleSlugs }) => {
 
   const today = new Date();
   const updateAccount = trpc.person.update.useMutation();
+  const confirmationDisclosure = useDisclosure();
+  const deleteMyAccount = trpc.user.deleteMyAccount.useMutation();
 
   const [name, setName] = useState(person?.name);
   const [gradYear, setGradYear] = useState(person?.gradYear);
@@ -77,7 +91,8 @@ const Account: NextPage<{ peopleSlugs: string[] }> = ({ peopleSlugs }) => {
             w="100%"
             flex={1}
             fontWeight={600}
-            textAlign="center">
+            textAlign="center"
+          >
             {name}
           </Heading>
           <Flex flex={1} justifyContent="flex-end">
@@ -90,18 +105,21 @@ const Account: NextPage<{ peopleSlugs: string[] }> = ({ peopleSlugs }) => {
           fontWeight={300}
           w="100%"
           textAlign="center"
-          textTransform="capitalize">
+          textTransform="capitalize"
+        >
           {person?.position}, Graduat{former ? "ed" : "ing"} {gradYear}
         </Text>
         <Text fontSize="2rem" fontWeight={300} w="100%" textAlign="center">
           {description ? `"${description}"` : <br />}
         </Text>
+
         <Flex flexDirection="column" alignItems="center" mt="3rem">
           <Heading
             mb="1rem"
             fontWeight={600}
             fontSize="2.5rem"
-            textAlign="center">
+            textAlign="center"
+          >
             Update your information
           </Heading>
 
@@ -133,7 +151,8 @@ const Account: NextPage<{ peopleSlugs: string[] }> = ({ peopleSlugs }) => {
                   gradYear === person?.gradYear &&
                   description === person?.description) ||
                 (!personSlugIsUnique(name || "") && name !== person?.name)
-              }>
+              }
+            >
               Save
             </Button>
             <Button
@@ -148,11 +167,63 @@ const Account: NextPage<{ peopleSlugs: string[] }> = ({ peopleSlugs }) => {
                 description === person?.description &&
                 gradYear === person?.gradYear &&
                 name === person?.name
-              }>
+              }
+            >
               Revert
             </Button>
           </Flex>
         </Flex>
+
+        <Modal {...confirmationDisclosure}>
+          <ModalOverlay />
+
+          <ModalContent bg="#ebeae5">
+            <ModalHeader>Are you REALLY REALLY SURE?</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text>Deleting your account will delete:</Text>
+              <Box ml="4">
+                <ul>
+                  <li>Your article submissions</li>
+                  <li>Your account</li>
+                  <li>Your profile</li>
+                </ul>
+              </Box>
+
+              <Text mt="2">
+                <b>THIS WILL FAIL IF YOU HAVE WRITTEN ANY ARTICLES.</b> Please
+                ask an exec to help delete your account.
+              </Text>
+
+              <Button
+                mt="4"
+                mb="2"
+                w="full"
+                isLoading={deleteMyAccount.isLoading}
+                onClick={() => {
+                  deleteMyAccount.mutateAsync().then(() => {
+                    signOut({
+                      callbackUrl: "/",
+                    });
+                  });
+                }}
+              >
+                I am sure, delete my account
+              </Button>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+
+        <Button
+          mt="16"
+          w="80"
+          mx="auto"
+          onClick={() => {
+            confirmationDisclosure.onOpen();
+          }}
+        >
+          I want to delete my account.
+        </Button>
       </RequiredUserWrapper>
     </Layout>
   );
