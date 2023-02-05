42557fbcea09ae17da0424d407ec337e02ecb07a
Nirjhor Nath <nirjhor.nath@gmail.com>
2023-02-05 12:21:12 -0500

    the happy radish

--- src/pages/api/og/person.tsx
+++ src/pages/api/og/person.tsx
@@ -27,7 +27,7 @@ export default async function handler(req: NextRequest) {
         }}
       >
         <img
-          src="https://theradishbths.com/images/sadish.png"
+          src="https://theradishbths.com/images/happyish.png"
           alt="Radish Logo"
           width={250}
           height={400}
