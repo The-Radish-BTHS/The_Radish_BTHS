e713237bcc57313f5830073c846ac82df0e653ad
Gilbert Zhang <gilbertzhang1208@gmail.com>
2024-10-10 17:26:27 -0400

    fix short links

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,10 @@
-import { get } from "@vercel/edge-config";
 import { next } from "@vercel/edge";
+import links from "./short-links.json";
 
 import { type NextRequest, NextResponse, NextFetchEvent } from "next/server";
 
-export async function middleware(
-  request: NextRequest,
-  context: NextFetchEvent
-) {
+export async function middleware(request: NextRequest) {
   const url = new URL(request.url);
-  const links = (await get("links")) as Record<string, string> | undefined;
 
   if (!links) {
     console.error("No links found. Is the edge config connected correctly?");
@@ -17,7 +13,10 @@ export async function middleware(
 
   if (url.pathname in links) {
     try {
-      return NextResponse.redirect(links[url.pathname], 302);
+      return NextResponse.redirect(
+        links[url.pathname as keyof typeof links],
+        302
+      );
     } catch (e) {
       console.error("Unable to redirect. Is the link a valid URL?");
       console.error(e);
@@ -30,6 +29,5 @@ export async function middleware(
 }
 
 export const config = {
-  // matches anything that doesnt start with public, static, api, _next, ..
-  matcher: "/((?!public|static|api|_next|robots.txt|favicon.ico).*)",
+  matcher: "/:shortlink",
 };
