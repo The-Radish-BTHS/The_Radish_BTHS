1441a9824b01f34635d2f37877de03f4306498fa
Gilbert Zhang <gilbertzhang1208@gmail.com>
2024-10-10 17:37:48 -0400

    a

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,5 +29,14 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/:shortlink",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
